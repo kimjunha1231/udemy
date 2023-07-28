cfa115188c76ee94a7e9cd48cc4871ba199635ca
junha kim <tto9011@naver.com>
2023-07-29 00:00:17 +0900

    21하는중

---
+++ JAVASCRIPT/FunctionRange/app.js
@@ -0,0 +1,1 @@
+let bird = "Scarlet Macaw";
---
+++ JAVASCRIPT/HigherOrderFunctions/app.js
@@ -0,0 +1,14 @@
+function callTwice(func) {
+  func();
+  func();
+}
+function callTenTimes(f) {
+  for (let i = 0; i < 10; i++) {
+    f();
+  }
+}
+function rollDie() {
+  const roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+}
+callTwice(rollDie);
