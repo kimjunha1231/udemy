dee731bca176f39dd267345699c6fba5d60f93d7
junha kim <tto9011@naver.com>
2023-07-29 00:00:11 +0900

    21 하는 중

---
+++ Block/app.js
@@ -0,0 +1,34 @@
+//let radius = 8;
+//if (radius > 0) {
+//  const PI = 3.14159;
+//  let msg = "HIII!";
+//}
+//console.log(radius);
+
+//for (let i = 0; i < 5; i++) {
+//  var msg = "ASKLDJAKLSJD";
+//  console.log(msg);
+//}
+//console.log(msg);
+//console.log(i);
+
+//function bankRobbery() {
+//  const heroes = ["Spiderman", "Wolverine", "Black Panther", "Batwonman"];
+//  function cryForHelp() {
+//    let color = "purple";
+//    function inner() {
+//      for (let hero of heroes) {
+//        console.log(`PLEASE HELP US, ${hero.toUpperCase()}`);
+//      }
+//    }
+//    inner();
+//  }
+//  cryForHelp();
+//}
+
+//bankRobbery();
+const add = function (x, y) {
+  return x + y;
+};
+
+const PI = 3.14;
