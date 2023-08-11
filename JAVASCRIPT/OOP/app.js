5aed8ba294a269e02194c0f9d7abc01d9fc4a01e
junha kim <tto9011@naver.com>
2023-08-11 14:53:14 +0900

    29끝

--- JAVASCRIPT/OOP/app.js
+++ JAVASCRIPT/OOP/app.js
@@ -1,8 +1,24 @@
-String.prototype.yell = function () {
-  return `OMG! ${this.toUpperCase()}!!!!!!!!!`;
-};
-"hello".yell();
+function hex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+function rgb(r, g, b) {
+  return `rgb(${r}, ${b} .${g})`;
+}
 
-Array.prototype.pop = function () {
-  return "SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!";
-};
+function makeColor(r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${b} .${g})`;
+  };
+  color.hex = function () {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  };
+  return color;
+}
+const firstColor = makeColor(35, 255, 150);
+firstColor.rgb();
